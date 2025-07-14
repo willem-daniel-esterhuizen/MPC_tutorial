2784063f453a8cca7c5a07d243a5b17a29b10614
willem_esterhuizen <willem.e@r2.energy>
2025-07-14 16:52:17 +0200

    WIP

--- MPC_tutorial.py
+++ MPC_tutorial.py
@@ -4,7 +4,7 @@ import numpy as np
 from casadi import *
 
 def plot_solution(x_tot, u):
-    # TODO: add initial and final dots on state space
+    # this takes lists of numpy arrays...
     fig = plt.figure(figsize=(10, 8))
     gs = fig.add_gridspec(3, 1)
     ax1 = fig.add_subplot(gs[0])
@@ -12,12 +12,13 @@ def plot_solution(x_tot, u):
     ax3 = fig.add_subplot(gs[2])
 
     # Plot state versus time
-    time = np.arange(len(x_tot) + 1)
-    x_1 = [x[0][0] for x in x_tot]
-    x_2 = [x[1][0] for x in x_tot]
+    x_1 = [x[0] for x in x_tot]
+    x_2 = [x[1] for x in x_tot]
 
+    # Duplicate the initial state so we can have a nice bar plot.
     x_1 = np.append(x_1[0], x_1)
     x_2 = np.append(x_2[0], x_2)
+    time = np.arange(len(x_tot) + 1) # because we duplicate the initial state
 
     ax1.step(time, x_1, 'b-', label='Position')
     ax1.step(time, x_2, 'r-', label='Velocity')
@@ -36,12 +37,15 @@ def plot_solution(x_tot, u):
     ax2.grid(True)
 
     # Plot the state in state space
-    ax3.plot([x[0][0] for x in x_tot], [x[1][0] for x in x_tot], 'k-', label='State')
+    ax3.plot(x_1, x_2, 'k-', label='State')
     ax3.set_xlabel('Position')
     ax3.set_ylabel('Velocity')
     ax3.legend()
     ax3.grid(True)
     
+    # Plot the initial and final states
+    ax3.plot([x_1[0], x_1[-1]], [x_2[0], x_2[-1]], 'k.')
+
     plt.tight_layout()
     plt.show()
     plt.show()
--- linear_example.py
+++ linear_example.py
@@ -41,11 +41,15 @@ for k in range(len(u)):
 plot_solution(x_tot, u)
 
 # # get the discrete-time system due to zero-order-hold control
-# A = np.array([[0, 1],[0, 0]])
-# B = np.array([[0],[1]])
-# C = np.array([[1, 0],[0, 1]])
-# D = np.array([[0, 0],[0, 0]])
-# dt = 0.1 # in seconds
-# discrete_system = cont2discrete((A, B, C, D), dt, method='zoh')
-# A_discrete, B_discrete, *_ = discrete_system
-# dynamics = {'A': A_discrete, 'B': B_discrete}
+
+import numpy as np
+from scipy.signal import cont2discrete
+
+A = np.array([[0, 1],[0, 0]])
+B = np.array([[0],[1]])
+C = np.array([[1, 0],[0, 1]])
+D = np.array([[0, 0],[0, 0]])
+dt = 0.1 # in seconds
+discrete_system = cont2discrete((A, B, C, D), dt, method='zoh')
+A_discrete, B_discrete, *_ = discrete_system
+dynamics = {'A': A_discrete, 'B': B_discrete}
